f41ed51f6425bc4c87dd36ce09b7b0956b6e3db7
Edward P <epotapov@purdue.edu>
2023-05-23 19:34:11 -0700

    finished entering in raw mode

--- pi.c
+++ pi.c
@@ -1,34 +1,60 @@
+/*
+ * pi: a text-editor
+ * Written by Edward P. 
+ *
+ * Based on the Kilo text-editor
+ * 
+ */
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
 
+
 struct termios o_termios;
 
+void error(const char *s) {
+	perror(s);
+	exit(1);
+}
+
 void disableRaw() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &o_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &o_termios) == -1)
+		error("tcsetattr error");
 }
 
 void enableRaw() {
-	tcgetattr(STDIN_FILENO, &o_termios);
+	if (tcgetattr(STDIN_FILENO, &o_termios) == -1)
+		error("tcgetattr error");
 	atexit(disableRaw);
 	struct termios raw = o_termios;
-	raw.c_iflag &= ~(IXON);
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+		error("tcsetattr error");
 }
 
 int main(int argc, char** argv) {
 	enableRaw();
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	while (1) {
+		char c = 0;
+		if (read(STDIN_FILENO, &c, 1) == -1)
+			error("read error");
 		if (iscntrl(c)) {
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		}
 		else {
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
+		if (c == 'q')
+			break;
 	}
 	return 0;
 }
